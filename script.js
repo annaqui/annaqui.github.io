619e93a58ad2573956185be3628efba91dd86139
Anna <anna@finecoffeeclub.com>
2016-09-18 20:41:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-//Scrollspy, to change active menu item when scrolling
-$('body').scrollspy({ target: '.navbar-default' })
-
-//To close the dropdown menu after clicking - from twbs Github issue
-$(document).on('click','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
-        $(this).collapse('hide');
-    }
-});
-
-//insert current year into footer
-$(document).getElementById("year").innerHTML = new Date().getFullYear();
+$(document).ready(function () {
+    //Scrollspy, to change active menu item when scrolling
+    $('body').scrollspy({ target: '.navbar-default' })
+    
+    //To close the dropdown menu after clicking - from twbs Github issue
+    $(document).on('click','.navbar-collapse.in',function(e) {
+        if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+            $(this).collapse('hide');
+        }
+    });
+    
+    //insert current year into footer
+    $(document).getElementById("year").innerHTML = new Date().getFullYear();
+};
